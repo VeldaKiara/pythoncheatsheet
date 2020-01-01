59fdb6d0a1242696dd6f96b32d0df2f4633c3260
Velda Kiara <veldakarimi@gmail.com>
2020-01-01 23:54:43 +0300

    added lists

--- pythoncheatsheet.py
+++ pythoncheatsheet.py
@@ -153,6 +153,132 @@ last_idx = verse.rfind('you')
 count = verse.count('you')
 print(message.format(length, first_idx, last_idx, count))
 
-#Data Strustures
+#Data Structures 
+#Data structures are containers that organize and group data types together in different ways.
+
+#Lists and Membership operators.
+#A list contains square brackets and are separated by commas. They also contain quotes as in the case of strings.
+#Examples of lists
+
+month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
+print (month[0])
+print(month [-1]) 
+#Lists are called when using their indexes. Python uses zero based indexing, 
+#this means that in indexing we always start using zero and not one hence when we call index zero in months you get the output as January
+#The negative number is used to call the last item on the index and -2 is used to call the second last number etc.
+
+#Slice and dice in lists
+#When using slicing, it is important to remember that the lower index is inclusive and the upper index is exclusive.
+print(month[:6]) #This print all the months from 0-5 which is Jan till June
+print(month[6:]) #This prints a list of elements from July to the last month.
+
+#Lists supports the len function, indexing and slicing.
+#In lists when you use the lenth function it will return the number of elements it holds, in a string it will return the number of characters it has.
+
+print(len(month))
+
+#Membership operators are In and not in.
+#In evaluates if object on the left side is included in object on the right side.
+#not in evaluates if object on the left side is not included in object on the right side.
+
+#more examples
+list_of_colors = ['red', 'maroon', 'purple', 'green']
+print(list_of_colors[1:2]) #The colon tells us to go from the starting value on the left of the colon up to, but not including, the element on the right.
+print(list_of_colors[:2])  #To start at the beginning 
+print(list_of_colors[1:]) #to get all items in the list
+
+#In and not in can be used in boolean. Boolean is a type that returns either true or false.
+print('this' in 'this is a string')
+print('isa' in  'this is a string')
+
+#Mutability and order
+#Mutability is whether or  not we can change an object once its created.
+#Lists are mutable while strings are immutable.
+
+list_of_cars = ['cadillac', 'porsche','ford']
+list_of_cars [0] = 'bimmer'
+print(list_of_cars)
+
+#key things to look out for is whether data types are ordered or mutable
+#Order is about whether the position of an element in the object can be used to access the element. Both strings and lists are ordered.
+
+#Quick Question 1
+#Use list indexing to determine how many days are in a particular month based on the integer variable month, 
+#and store that value in the integer variable num_days. For example, if month is 8, 
+#num_days should be set to 31, since the eighth month, August, has 31 days.
+#Remember to account for zero-based indexing!
+
+month = 8
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+#use list indexing to determine the number of days in month
+number_of_days = days_in_month[month - 1] #you can use this as the solution
+num_days = days_in_month[-5] #this can also be used as a solution. There are many ways this can be done.
+print(num_days)
+print(number_of_days)
+
+#Quick Question 2
+#Select the three most recent dates from this list using list slicing notation. Hint: negative indexes work in slices!
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+                 
+                 
+# TODO: Modify this line so it prints the last three elements of the list
+print(eclipse_dates[7:10])  #This can be used as the solution but it does not use negative.
+print (eclipse_dates[-3:])
+
+#Useful functions of a list.
+#sorted() returns a copy of a list in order from smallest to largest, leaving the list unchanged.
+#min() returns the smallest element in a list
+#max() returns the greatest element of the list
+#len() returns how many elements are in a list.
+
+#Useful functions of a list 2
+#Join is a string method that takes a list of strings as an argument, 
+#and returns a string consisting of the list elements joined by a separator string.
+# '/n' new line
+
+new_str = "\n".join(["fore", "aft", "starboard", "port"])
+print(new_str)
+
+#Using a hyphen
+name = "-".join(["García", "O'Kelly"])
+print(name)
+
+#Append method adds items to a list.
+alphabets= ['a', 'b', 'c', 'd']
+alphabets.append('z')
+print(alphabets)
+
+#Quick question 1 len, max, min and  lists.
+#What would the output of the following code be? (Treat the comma in the multiple choice answers as newlines.
+a = [1, 5, 8]
+b = [2, 6, 9, 10]
+c = [100, 200]
+
+print(max([len(a), len(b), len(c)]))
+print(min([len(a), len(b), len(c)]))
+
+#Quick question 2  sorted, join and lists. 
+#What would the output of the following code be? (Treat the comma in the multiple choice answers as newlines.)
+names = ["Carol", "Albert", "Ben", "Donna"]
+print(" & ".join(sorted(names)))
+
+#Quick question 3 Append and list
+#What would the output of the following code be? (Treat the commas in the multiple choice answers as newlines.)
+names = ["Carol", "Albert", "Ben", "Donna"]
+names.append("Eugenia")
+print(sorted(names))
+#NB Data structures are containers that include different dat types. A list is a data structure. All data structures are data types.
+#NB list are ordered and mutable
+
+
+
+
+
+
+
+
 
 
